7e9200ead71153b1f3bf2c5044ed72216dd484ae
Daniel Cannon <daniel@danielcannon.co.uk>
2015-04-18 23:12:53 +0100

    Update changefeed queries to retry if the query fails.

--- ws_changes.go
+++ ws_changes.go
@@ -6,56 +6,62 @@ import (
 )
 
 func allChanges(ch chan interface{}) {
-	res, err := r.Db("todo").Table("items").Changes().Run(session)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	// Use goroutine to wait for changes. Prints the first 10 results
 	go func() {
-		var response r.WriteChanges
-		for res.Next(&response) {
-			ch <- response
-		}
+		for {
+			res, err := r.Db("todo").Table("items").Changes().Run(session)
+			if err != nil {
+				log.Fatalln(err)
+			}
 
-		if res.Err() != nil {
-			log.Fatalln(res.Err())
+			var response r.WriteChanges
+			for res.Next(&response) {
+				ch <- response
+			}
+
+			if res.Err() != nil {
+				log.Println(res.Err())
+			}
 		}
 	}()
 }
 func activeChanges(ch chan interface{}) {
-	res, err := r.Db("todo").Table("items").Filter(r.Row.Field("Status").Eq("active")).Changes().Run(session)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	// Use goroutine to wait for changes. Prints the first 10 results
 	go func() {
-		var response r.WriteChanges
-		for res.Next(&response) {
-			ch <- response
-		}
+		for {
+			res, err := r.Db("todo").Table("items").Filter(r.Row.Field("Status").Eq("active")).Changes().Run(session)
+			if err != nil {
+				log.Fatalln(err)
+			}
 
-		if res.Err() != nil {
-			log.Fatalln(res.Err())
+			var response r.WriteChanges
+			for res.Next(&response) {
+				ch <- response
+			}
+
+			if res.Err() != nil {
+				log.Println(res.Err())
+			}
 		}
 	}()
 }
 func completedChanges(ch chan interface{}) {
-	res, err := r.Db("todo").Table("items").Filter(r.Row.Field("Status").Eq("complete")).Changes().Run(session)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	// Use goroutine to wait for changes. Prints the first 10 results
 	go func() {
-		var response r.WriteChanges
-		for res.Next(&response) {
-			ch <- response
-		}
+		for {
+			res, err := r.Db("todo").Table("items").Filter(r.Row.Field("Status").Eq("complete")).Changes().Run(session)
+			if err != nil {
+				log.Fatalln(err)
+			}
 
-		if res.Err() != nil {
-			log.Fatalln(res.Err())
+			var response r.WriteChanges
+			for res.Next(&response) {
+				ch <- response
+			}
+
+			if res.Err() != nil {
+				log.Println(res.Err())
+			}
 		}
 	}()
 }
